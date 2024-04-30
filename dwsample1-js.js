34d4fc057aed42bccd9a11fbebf6f08b3df7e632
votn <votniccus99@gmail.com>
2024-04-30 11:22:30 +0700

    second commit

--- dwsample1-js.js
+++ dwsample1-js.js
@@ -3,4 +3,4 @@
 
 const number = prompt('Enter the number: ');
 const result = Math.sqrt(number);
-console.log(`The square root of ${number} is ${result}`);
+console.log(`The square root of ${number} is ${result} Please are this changed`);
